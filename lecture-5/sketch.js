ae7dfd97ef16325a78db6bfc81669dcdb56e64ab
NiksZilite <150466203+KayPawzlv@users.noreply.github.com>
2024-03-28 11:32:28 +0200

    Lecture 5 (28 March)

---
+++ lecture-5/sketch.js
@@ -0,0 +1,26 @@
+let img;
+
+// Load the image and create a p5.Image object.
+function preload() {
+  img = loadImage("./photo.jpg");
+}
+
+function setup(){
+    createCanvas(
+        windowWidth, windowHeight
+        );
+}
+
+function draw(){
+    image(img, 0, 0, width, height);
+
+
+  if(mouseX < width / 2 && mouseY < height /2){
+    circle(mouseX,mouseY, 50)
+  }
+
+  if(mouseX > width / 2 && mouseY > height /2){
+    square(mouseX,mouseY, 50)
+  }
+}
+š
